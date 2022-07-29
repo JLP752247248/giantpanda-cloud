16c56bc67e2f0e69eebd236da3eef5cec12e6cf8
jianglp <jianglp@t3go.cn>
2022-07-29 18:20:13 +0800

    spring-security-jwt

--- panda-auth/src/main/java/com/panda/auth/config/ApplicationSecurity.java
+++ panda-auth/src/main/java/com/panda/auth/config/ApplicationSecurity.java
@@ -2,6 +2,7 @@ package com.panda.auth.config;
 
 import com.panda.auth.config.authorization.AnyMatchBased;
 import com.panda.auth.config.authorization.CustomFilterSecurityInterceptor;
+import com.panda.auth.config.authorization.JwtAuthenticationTokenFilter;
 import com.panda.auth.config.authorization.RealFilterInvocationSecurityMetadataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,11 +13,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.rememberme.InMemoryTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -40,6 +49,7 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
                 .rememberMeServices(persistentTokenBasedRememberMeServices())
                 .tokenValiditySeconds(14 * 24 * 60 * 60);
         //加入自定义的权限拦截器
+        http.addFilterAfter(jwtAuthTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         http.addFilterAfter(customFilterSecurityInterceptor(), FilterSecurityInterceptor.class);
     }
 
@@ -49,6 +59,11 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
     private RememberMeServices persistentTokenBasedRememberMeServices() {
         TokenBasedRememberMeServices tokenBasedRememberMeServices = new TokenBasedRememberMeServices(DEFAULT_REMEMBER_ME_KEY, userDetailsService);
         return tokenBasedRememberMeServices;
+    }
+
+    private OncePerRequestFilter jwtAuthTokenFilter() {
+        JwtAuthenticationTokenFilter jwtAuthTokenFilter = new JwtAuthenticationTokenFilter();
+        return jwtAuthTokenFilter;
     }
 
     @Bean
@@ -63,4 +78,5 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
         filterSecurityInterceptor.setAuthenticationManager(authenticationManager());
         return filterSecurityInterceptor;
     }
+
 }
