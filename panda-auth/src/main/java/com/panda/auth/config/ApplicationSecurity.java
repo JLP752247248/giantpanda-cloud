315a659617e22e20988246f43b0fd3e9e3758b47
jianglp <jianglp@t3go.cn>
2022-08-08 15:33:08 +0800

    oauth2-github

--- panda-auth/src/main/java/com/panda/auth/config/ApplicationSecurity.java
+++ panda-auth/src/main/java/com/panda/auth/config/ApplicationSecurity.java
@@ -1,24 +1,38 @@
 package com.panda.auth.config;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.nacos.common.utils.MD5Utils;
 import com.panda.auth.config.authorization.AnyMatchBased;
 import com.panda.auth.config.authorization.CustomFilterSecurityInterceptor;
 import com.panda.auth.config.authorization.JwtAuthenticationTokenFilter;
 import com.panda.auth.config.authorization.RealFilterInvocationSecurityMetadataSource;
+import com.panda.auth.util.TokenUtil;
+import com.panda.common.mvc.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 
 /**
  * @Author: JLP
@@ -39,9 +53,9 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .failureForwardUrl("/login/failure")
-                .successForwardUrl("/login/success")
+                .oauth2Login()
+                .loginPage("/login/oauth2")
+                .successHandler(authenticationSuccessHandler())
                 .permitAll()
                 .and()
                 .logout()
@@ -50,11 +64,36 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
                 .key(DEFAULT_REMEMBER_ME_KEY)
                 .rememberMeServices(persistentTokenBasedRememberMeServices())
                 .tokenValiditySeconds(14 * 24 * 60 * 60)
-                ;
+        ;
         //加入自定义的权限拦截器
         http.addFilterAfter(jwtAuthTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         //http.addFilterAfter(customFilterSecurityInterceptor(), FilterSecurityInterceptor.class);
     }
+
+    /**
+     * 登陆成功后，生成token信息，并返回给前端
+     * @return
+     */
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        AuthenticationSuccessHandler authenticationSuccessHandler = new AuthenticationSuccessHandler() {
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                String md5 = "";
+                try {
+                    UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+                    md5 = MD5Utils.md5Hex(token.toString().getBytes());
+                    TokenUtil.tokenMap.put(md5, token);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                response.setContentType("application/json;charset=utf-8");
+                Response<String> resp =Response.createSuc(md5,"登录成功");
+                response.getWriter().write(JSONObject.toJSONString(resp));
+            }
+        };
+        return authenticationSuccessHandler;
+    }
+
 
     @Autowired
     private UserDetailsService userDetailsService;
--- panda-auth/src/main/java/com/panda/auth/user/controller/LoginRestController.java
+++ panda-auth/src/main/java/com/panda/auth/user/controller/LoginRestController.java
@@ -9,13 +9,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping(value = "/login")
 @Slf4j
 public class LoginRestController {
@@ -36,9 +35,7 @@ public class LoginRestController {
      */
     @PostMapping("/failure")
     public ResponseEntity loginFailure() {
-
         return new ResponseEntity("登录失败了，老哥",HttpStatus.UNAUTHORIZED);
-
     }
 
     /**
@@ -47,11 +44,19 @@ public class LoginRestController {
      * @return the rest
      */
     @PostMapping("/success")
-    public ResponseEntity loginSuccess() {
-        // 登录成功后用户的认证信息 UserDetails会存在 安全上下文寄存器 SecurityContextHolder 中
-        //User principal = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        //String username = principal.getUsername();
-        return new ResponseEntity("登录成功",HttpStatus.OK);
+    public ResponseEntity<String> loginSuccess() {
+        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        return new ResponseEntity(token.toString(),HttpStatus.OK);
+    }
+
+    /**
+     * 登录失败返回 401 以及提示信息.
+     *
+     * @return the rest
+     */
+    @GetMapping("/oauth2")
+    public String oAuthLogin() {
+        return "login";
     }
 
 }
