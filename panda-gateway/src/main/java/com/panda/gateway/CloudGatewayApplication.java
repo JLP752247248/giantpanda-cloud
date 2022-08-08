665e8a9e7143a2ef01bce3cfea94049ce054490a
JLP752247248 <752247248@qq,com>
2022-08-08 22:59:23 +0800

    ssl-pk

--- panda-gateway/src/main/java/com/panda/gateway/CloudGatewayApplication.java
+++ panda-gateway/src/main/java/com/panda/gateway/CloudGatewayApplication.java
@@ -1,7 +1,9 @@
 package com.panda.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -11,6 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class CloudGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(CloudGatewayApplication.class, args);
